5e97a3cf762b83649784650f448a79a8a327af50
Yiğit <yigitahmetkara01@gmail.com>
2026-01-26 13:05:37 +0300

    Initial Commit

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Yiğit Ahmet KARA on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
